2177f820d437c27218cf5ac51966657e584168d8
hwang-58hw <hwang.58hw@gmail.com>
2022-08-28 11:03:13 +0700

    vercel 16

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { TrackerContext, TrackerProvider } from "./vendors";
+
+const RenderContent = () => {
+  const { initTracker, startTracking, logEvent, logIssue } =
+    React.useContext(TrackerContext);
+
+  React.useEffect(() => {
+    initTracker();
+    startTracking();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <button
+        onClick={() => {
+          logEvent({
+            name: "Event",
+            data: {
+              click_type: "event",
+            },
+          });
+
+          logIssue({
+            name: "Issue",
+            data: {
+              click_type: "issue",
+            },
+          });
+        }}
+      >
+        Click Shop
+      </button>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Deploy times: 16</h1>
+      <TrackerProvider
+        config={{
+          projectKey: "IPQJ2ytBAXqwnwxtPmgh",
+        }}
+      >
+        <RenderContent />
+      </TrackerProvider>
+    </div>
+  );
+}
+
+export default App;
