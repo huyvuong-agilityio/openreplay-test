c66a15f4a382a40a31024c8467c7d81f8d5fc051
Huy Vuong <huy.vuong@asnet.com.vn>
2022-09-07 10:37:42 +0700

    update experimental feature

--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
-    // do not leak source-maps in Vercel production deployments
-    // but keep them in Vercel preview deployments with generated urls
-    // for better dev experience
-    const isVercelProdDeploy = process.env.VERCEL_ENV === "production";
-
-    if (isVercelProdDeploy) {
-      return {
-        beforeFiles: [
-          {
-            source: "/:path*.map",
-            destination: "/404",
-          },
-        ],
-      };
-    }
-
-    return [];
+  experimental: {
+    productionBrowserSourceMaps: true,
   },
 };
 
