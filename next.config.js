9b5a53298e33058116a9affa56de65601e52919f
Huy Vuong <huy.vuong@asnet.com.vn>
2022-09-07 10:06:18 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,26 @@ const nextConfig = {
       ],
     },
   },
+  rewrites: async () => {
+    // do not leak source-maps in Vercel production deployments
+    // but keep them in Vercel preview deployments with generated urls
+    // for better dev experience
+    const isVercelProdDeploy = process.env.VERCEL_ENV === "production";
+
+    if (isVercelProdDeploy) {
+      return {
+        beforeFiles: [
+          {
+            source: "/:path*.map",
+            destination: "/404",
+          },
+        ],
+      };
+    }
+
+    return [];
+  },
+  // ...
 };
 
 module.exports = nextConfig;
