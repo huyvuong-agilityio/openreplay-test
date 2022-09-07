2bf77367bf2e705e225a949afb8628f249ae72cd
Huy Vuong <huy.vuong@asnet.com.vn>
2022-09-07 09:58:51 +0700

    webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
+  webpack: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: "pre",
+          use: ["source-map-loader"],
+        },
+      ],
+    },
+  },
 };
 
 module.exports = nextConfig;
