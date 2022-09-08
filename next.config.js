3bbbc428ea9700e28df895a91bb19aeec2a993c8
Huy Vuong <huy.vuong@asnet.com.vn>
2022-09-08 10:16:13 +0700

    cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   swcMinify: true,
   productionBrowserSourceMaps: true,
+  async headers() {
+    return [
+      {
+        source: "./",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
