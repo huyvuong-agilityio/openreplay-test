ce0a35dbe05b830e7243e54a0f1b9859a0e5399e
Huy Vuong <huy.vuong@asnet.com.vn>
2022-09-08 17:08:24 +0700

    daf

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
   productionBrowserSourceMaps: true,
-  webpack: {
-    devtool: "source-map",
-  },
 };
 
 module.exports = nextConfig;
