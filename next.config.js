5b09502e874994acbe87b6da182b0c79be905ab4
Huy Vuong <huy.vuong@asnet.com.vn>
2022-09-08 10:19:16 +0700

    fkeofa

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
         headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
       },
     ];
