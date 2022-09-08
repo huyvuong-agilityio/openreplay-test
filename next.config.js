5dd1b0219a2fb7d875e086d02bd0363455e08af6
Huy Vuong <huy.vuong@asnet.com.vn>
2022-09-08 10:17:20 +0700

    cors

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "./",
+        source: "/api/:path*",
         headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
       },
     ];
